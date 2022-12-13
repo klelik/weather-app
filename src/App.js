16151a0fe2dc3b8ebbec60483e33fee36fd1f469
klelik <klelika1998@gmail.com>
2022-12-13 15:23:44 +0000

    Conditional Rendering Working
    Geolocation breaking -> Tested so when we have already fetched the current weather in the currentPossition its working fine.
    When trying to render the currentPosition for the first time it breaks because current position is null maybe because api is slow and we get the data of the current location after rendering so we render null and thats why it breaks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Forecast from "./components/forecast/forecast";
 import React from "react";
 import { useState } from "react";
 import { WEATHER_API_KEY, WEATHER_API_URL, FORECAST_API_URL } from "./api";
+import { render } from "@testing-library/react";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -13,8 +14,11 @@ function App() {
   const [lng, setLng] = useState(null);
   const [status, setStatus] = useState(true);
   const [currentPosition, setCurrentPosition] = useState(null);
+  const [currentActive, setCurrentActive] = useState(false);
 
   const getLocation = () => {
+    //find current lat and long
+    setCurrentActive(true);
     if (!navigator.geolocation) {
       setStatus("Geolocation is not supported by your browser");
     } else {
@@ -29,27 +33,25 @@ function App() {
           setStatus("Unable to retrieve your location");
         }
       );
+      //fetching weather api after finding geolocation 
+      fetch(
+        `${WEATHER_API_URL}lat=${lati}&lon=${lng}&appid=${WEATHER_API_KEY}&units=metric`
+      ).then(async (response) => {
+        const currentResponse = await response.json(); //
+        setCurrentPosition({ city: currentResponse.name, ...currentResponse });
+        //  console.log(currentPosition);   //it fetches from the api correctly the weather at my location
+        console.log(currentPosition);
+      });
     }
-  };
-  // console.log(lati)
-  // console.log(lng)
-
-  const fetchCurrent = () => {
-    fetch(
-      `${WEATHER_API_URL}lat=${lati}&lon=${lng}&appid=${WEATHER_API_KEY}&units=metric`
-    ).then(async (response) => {
-      const currentResponse = await response.json(); //
-      setCurrentPosition({ city: currentResponse.name, ...currentResponse });
-      //  console.log(currentPosition);   //it fetches from the api correctly the weather at my location
-    });
   };
 
   const handleOnSearchChange = (searchData) => {
+    setCurrentActive(false);
     //a function to console log the results which we call on <Search />
     console.log(searchData); //we get the following every time we search for a new city  {value: '51.507222222 -0.1275', label: 'London,GB'}
-    const [lat, lon] = searchData.value.split(" ");
+    const [lat, lon] = searchData.value.split(" ");   //we need the lat and lon from searchData
 
-    const currentWeatherFetch = fetch(
+    const currentWeatherFetch = fetch(  //fetching the weather of the city we searched using the lat and lon of the specific city
       `${WEATHER_API_URL}lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
     const forecastFetch = fetch(
@@ -66,23 +68,47 @@ function App() {
       .catch((err) => console.log.log(err));
   };
   console.log(currentWeather);
-  return (
-    <div className="App ">
+
+  if(currentActive){
+    console.log("test1");
+    return(
+      <div className="App">
       <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather} />}{" "}
+        {/* {currentPosition ? (<div>Loading</div>): ( <CurrentWeather data={currentPosition} />)} */}
+      {currentPosition && <CurrentWeather data={currentPosition} />}{" "}
       {/* //that means if the current weather is not null show <CurrentWeather /> otherwise dont show anything  */}
-      {/* To show current weather <CurrentWeather data={currentWeather} */}
       <button
         onClick={() => {
           getLocation();
-          fetchCurrent();
         }}
       >
         Get Location
       </button>
       {/* <Forecast data={forecast}    /> */}
     </div>
-  );
+    )
+  } else {
+    console.log("test2");
+    return (
+      <div className="App">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {/* //that means if the current weather is not null show <CurrentWeather /> otherwise dont show anything  */}
+      {/* To show current weather <CurrentWeather data={currentPosition} */}
+      <button
+        onClick={() => {
+          getLocation();
+        }}
+      >
+        Get Location
+      </button>
+
+      {/* <Forecast data={forecast}    /> */}
+    </div>
+    )
+  }
+
 }
+
 
 export default App;
